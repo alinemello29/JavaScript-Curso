a8d622f1988c818b9cf660f7bfd4c9d6e4b58a25
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2024-04-18 19:44:10 -0300

    Update var.js

--- Variaveis/Aula-1/var.js
+++ Variaveis/Aula-1/var.js
@@ -1,1 +1,13 @@
+// Solicita ao usuário que digite três números e armazena esses valores em variáveis
+let numero1 = parseFloat(prompt("Digite o primeiro número:"));
+let numero2 = parseFloat(prompt("Digite o segundo número:"));
+let numero3 = parseFloat(prompt("Digite o terceiro número:"));
 
+
+
+/
+// Calcula a média dos três números
+let media = (numero1 + numero2 + numero3) / 3;
+
+// Exibe a média calculada para o usuário
+alert("A média dos números digitados é: " + media);
