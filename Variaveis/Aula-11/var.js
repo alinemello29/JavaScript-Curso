0d705f58e6380336bc84b7a427a41efc6836ef3a
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2024-05-13 17:06:45 -0300

    Create var.js

---
+++ Variaveis/Aula-11/var.js
@@ -0,0 +1,10 @@
+var exibeMensagem = function() { 
+    var mensagemForaDoIf = 'Caelum'; 
+    if(true) { 
+        var mensagemDentroDoIf = 'Alura'; 
+        console.log(mensagemDentroDoIf)// Alura ;
+    } 
+    console.log(mensagemForaDoIf); // Caelum 
+
+    console.log(mensagemDentroDoIf); // Alura 
+}
